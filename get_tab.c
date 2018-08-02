6cdbe7cf6b012bdf3cee668e4f717a58dd468958
Alexandre PELISSIER <apelissi@e2r4p11.42.fr>
2018-08-02 17:00:51 +0200

    GG

--- get_tab.c
+++ get_tab.c
@@ -1,28 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   FDF.h                                              :+:      :+:    :+:   */
+/*   get_tab.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apelissi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/31 11:29:34 by apelissi          #+#    #+#             */
-/*   Updated: 2018/08/02 16:44:32 by apelissi         ###   ########.fr       */
+/*   Created: 2018/08/02 16:41:41 by apelissi          #+#    #+#             */
+/*   Updated: 2018/08/02 17:00:31 by apelissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#	ifndef	FDF_H
-#	define	FDF_H
+#include "FDF.h"
 
-#include "mlx.h"
-#include "stdlib.h"
-#include "unistd.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#define ACC 1000
 
-int fil(float x, float y, float a, float b);
-int	**get_tab(int fd);
-int	get_next_line(const int fd, char **line);
+int		*get_line(char *str)
+{
+	int	*numbers;
+	int nb;
+	
 
-#endif
+}
+
+int		**get_tab(int fd)
+{
+	char	*line;
+	int		**tab;
+	int		x;
+	int		y;
+
+	get_next_line
+}
